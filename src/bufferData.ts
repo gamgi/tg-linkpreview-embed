37d162359376413d366a5c3505cf438add2ffe6c
Pietu R <pietu_r@gmx.com>
2020-04-18 17:23:34 +0300

    buffer data

---
+++ src/bufferData.ts
@@ -0,0 +1,61 @@
+import type { Vec4 } from './types';
+// prettier-ignore
+export const positions = [
+  // Front face
+  -1.0, -1.0,  1.0,
+  1.0, -1.0,  1.0,
+  1.0,  1.0,  1.0,
+  -1.0,  1.0,  1.0,
+
+  // Back face
+  -1.0, -1.0, -1.0,
+  -1.0,  1.0, -1.0,
+  1.0,  1.0, -1.0,
+  1.0, -1.0, -1.0,
+
+  // Top face
+  -1.0,  1.0, -1.0,
+  -1.0,  1.0,  1.0,
+  1.0,  1.0,  1.0,
+  1.0,  1.0, -1.0,
+
+  // Bottom face
+  -1.0, -1.0, -1.0,
+  1.0, -1.0, -1.0,
+  1.0, -1.0,  1.0,
+  -1.0, -1.0,  1.0,
+
+  // Right face
+  1.0, -1.0, -1.0,
+  1.0,  1.0, -1.0,
+  1.0,  1.0,  1.0,
+  1.0, -1.0,  1.0,
+
+  // Left face
+  -1.0, -1.0, -1.0,
+  -1.0, -1.0,  1.0,
+  -1.0,  1.0,  1.0,
+  -1.0,  1.0, -1.0,
+];
+
+// prettier-ignore
+export const faceColors: Vec4[] = [
+  [1.0,  1.0,  1.0,  1.0],    // Front face
+  [1.0,  0.0,  0.0,  1.0],    // Back face
+  [0.0,  1.0,  0.0,  1.0],    // Top face
+  [0.0,  0.0,  1.0,  1.0],    // Bottom face
+  [1.0,  1.0,  0.0,  1.0],    // Right face
+  [1.0,  0.0,  1.0,  1.0],    // Left face
+];
+
+// defines each face as two triangles, using indices into
+// the vertex array to specify each triangle's position.
+// prettier-ignore
+export const triangleVertexIndices = [
+  0,  1,  2,      0,  2,  3,    // front
+  4,  5,  6,      4,  6,  7,    // back
+  8,  9,  10,     8,  10, 11,   // top
+  12, 13, 14,     12, 14, 15,   // bottom
+  16, 17, 18,     16, 18, 19,   // right
+  20, 21, 22,     20, 22, 23,   // left
+];
