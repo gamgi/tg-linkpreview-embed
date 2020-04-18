c132dcf099521dcc8124eb621570547b5907a7e3
Pietu R <pietu_r@gmx.com>
2020-04-18 17:30:39 +0300

    draw scene

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+import { initBuffers } from './buffers';
+import shader, { initShaderProgram } from './shader';
+import { drawScene } from './draw';
+import type { Buffers } from './types';
+import type { ProgramInfo } from './shader/types';
 
 function main(): void {
   const canvas = getCanvas();
@@ -7,6 +12,10 @@ function main(): void {
     throw new Error('Unable to initialize WebGL. Your browser or machine may not support it.');
   }
 
+  const programInfo = initShaderProgram(gl, shader.vs, shader.fs);
+  const buffers = initBuffers(gl, programInfo);
+  setLoading(false);
+  renderLoop(gl, programInfo, buffers);
 }
 function getCanvas(): HTMLCanvasElement {
   const container = document.querySelector('#canvas-container');
@@ -25,6 +34,19 @@ function setLoading(loading: boolean): void {
   }
 }
 
+function renderLoop(gl: WebGL2RenderingContext, programInfo: ProgramInfo, buffers: Buffers): void {
+  let then = performance.now() * 0.001 - 0.01;
+  function render(now: number): void {
+    // timing
+    now *= 0.001;
+    const timeDelta = now - then;
+    then = now;
+
+    drawScene(gl, programInfo, buffers, timeDelta);
+    requestAnimationFrame(render);
+  }
+
+  requestAnimationFrame(render);
 }
 
 window.onload = main;
